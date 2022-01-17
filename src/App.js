3efc2b62a0434739fc5a1c44aefc995c1ea82d31
Swarnali Saha <swarnalisaha2000.ss@gmail.com>
2022-01-17 23:22:12 +0530

    Event handling in functional components

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick';
 
 
 function App() {
     return ( 
         <div className = "App">
+        <FunctionClick></FunctionClick>
         {/*<Greet name="riya" age="10">
         <p>This is a children and they are adults</p></Greet>
         <Greet name="rima" age="15">
-    <button>Action</button></Greet>*/}
+    <button>Action</button></Greet>
         <Greet name="rohit" age="16" ></Greet>
         <Welcome name="riya" age="10"></Welcome>
-       {/* <Welcome name="rima" age="15"></Welcome>
+       <Welcome name="rima" age="15"></Welcome>
         <Counter/>
 <Message />*/}
         </div>
