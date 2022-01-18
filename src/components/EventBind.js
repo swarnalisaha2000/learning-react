adec5a51ce25e3df10525cccf097457c96f456ee
Swarnali Saha <swarnalisaha2000.ss@gmail.com>
2022-01-18 11:22:32 +0530

    Event handler binding using arrow function

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -22,7 +22,8 @@ class EventBind extends Component {
         return (
             <div>
                 <div>{this.state.message}</div>
-                 <button onClick={this.clickHandler.bind(this)}> Click</button>
+                 {/* <button onClick={this.clickHandler.bind(this)}> Click</button> */}
+                 <button onClick={() => this.clickHandler()}  > Click</button>
             </div>
         )
     }
