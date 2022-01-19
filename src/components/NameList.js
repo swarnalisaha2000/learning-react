5bdb4edbad1adcb5f325f3dfbafa292f3e83851a
Swarnali Saha <swarnalisaha2000.ss@gmail.com>
2022-01-19 13:36:05 +0530

    INdex as key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,38 +2,38 @@ import React from 'react'
 import PersonPer from './PersonPer'
 
 function NameList() {
-    // const name = ['Bruce', 'Ammer', 'Diana','Riya']
-    // const names = name.map(x => <h2>{x}</h2>)
-    // return (
-    //     <div>{names}</div>
-    // )
-
-    const person = [
-        {
-            name:"John",
-            age:31, 
-            city:"New York"
-        },
-        {
-            name:"Mark",
-            age:45, 
-            city:"New Jersey"
-        },
-        {
-            name:"Michael",
-            age:30, 
-            city:"UK"
-        },
-        {
-            name:"Sam",
-            age:23, 
-            city:"Zambia"
-        }
-    ]
-    const personList = person.map( persons => 
-    <PersonPer key={persons.age} persons={persons}/>
+    const name = ['Bruce', 'Ammer', 'Diana','Riya']
+    const names = name.map((x,index) => <h2 key={index}>{index} {x}</h2>)
+    return (
+        <div>{names}</div>
     )
-    return (<div>{personList}</div>)
+
+    // const person = [
+    //     {
+    //         name:"John",
+    //         age:31, 
+    //         city:"New York"
+    //     },
+    //     {
+    //         name:"Mark",
+    //         age:45, 
+    //         city:"New Jersey"
+    //     },
+    //     {
+    //         name:"Michael",
+    //         age:30, 
+    //         city:"UK"
+    //     },
+    //     {
+    //         name:"Sam",
+    //         age:23, 
+    //         city:"Zambia"
+    //     }
+    // ]
+    // const personList = person.map( persons => 
+    // <PersonPer key={persons.age} persons={persons}/>
+    // )
+    // return (<div>{personList}</div>)
 }
 
 export default NameList
