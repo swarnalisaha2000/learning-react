06d6aa7a72ebaf477df1ab87256228df0b0cc803
Swarnali Saha <swarnalisaha2000.ss@gmail.com>
2022-04-26 11:29:58 +0530

    context-part 2

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './Usercontext'
 
 export class ComponentE extends Component {
+
+  static contextType = UserContext
   render() {
     return (
-        <ComponentF />
+      <div>Component E renders {this.context}
+        <ComponentF /> 
+      </div>
     )
   }
 }
 
+//ComponentE.contextType = UserContext
 export default ComponentE
--- src/components/Usercontext.js
+++ src/components/Usercontext.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('Tota') // Here Tota is Default value of Context parameter
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export { UserProvider, UserConsumer }
+
+export default UserContext
